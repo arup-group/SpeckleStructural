7878749b04a2e09e735ac64447cf444984d96296
Nicolaas Burgers <nic.burgers@arup.com>
2021-04-15 18:15:56 +1000

    Changes to reflect the changes in the GSAInterfaces library by the SpeckleGSA app.

---
+++ SpeckleStructuralGSA.Test/Other/GsaResultParams.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpeckleGSAInterfaces;
+
+namespace SpeckleStructuralGSA.Test
+{
+  //Copied directly from SpeckleGSA
+  public class GsaResultParams : IGSAResultParams
+  {
+    public int ResHeader { get; set; }
+    public int Flags { get; set; }
+    public List<string> Keys { get; set; } //Field/column headers
+    public string Keyword { get; set; }  //TODO: is this still necessary?
+
+    public GsaResultParams(int resHeader, int flags, List<string> keys)
+    {
+      this.ResHeader = resHeader;
+      this.Flags = flags;
+      this.Keys = keys;
+    }
+
+    public GsaResultParams(string keyword, int resHeader, int flags, List<string> keys)
+    {
+      this.ResHeader = resHeader;
+      this.Flags = flags;
+      this.Keys = keys;
+      if (!string.IsNullOrEmpty(keyword))
+      {
+        this.Keyword = keyword;
+      }
+    }
+  }
+}
